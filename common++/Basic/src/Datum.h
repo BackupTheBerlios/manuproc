5d261731236ff937a264885f0e2bcaa566b98e3b
christof <christof>
2002-04-19 15:16:36 +0000

    build also without pg

--- common++/Basic/src/Datum.h
+++ common++/Basic/src/Datum.h
@@ -1,4 +1,4 @@
-/* $Id: Datum.h,v 1.6 2002/04/19 06:23:22 christof Exp $ */
+/* $Id: Datum.h,v 1.7 2002/04/19 15:16:36 christof Exp $ */
 /*  libcommonc++: ManuProC's main OO library
  *  Copyright (C) 1998-2000 Adolf Petig GmbH & Co. KG, written by Christof Petig
  *
