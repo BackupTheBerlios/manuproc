ff46af3212d58f615a1dec3b3206f7ce565a831a
christof <christof>
2002-07-08 08:09:15 +0000

    *** empty log message ***

---
+++ common++/BaseObjects/ManuProcEntity.h
@@ -0,0 +1,45 @@
+/* $Id: ManuProcEntity.h,v 1.1 2002/07/08 08:09:15 christof Exp $ */
+/*  libcommonc++: ManuProC's main OO library
+ *  Copyright (C) 2002 Adolf Petig GmbH & Co. KG
+ *  written by Jacek Jakubowski, Christof Petig, Malte Thoma
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+
+
+#ifndef MANU_PROC_ENTITY
+#define MANU_PROC_ENTITY
+
+#include<Aux/Handles.h>
+
+
+class ManuProcEntity : public HandleContent
+{
+public:
+ typedef long int ID;
+ 
+protected:
+ ID entityid;
+ 
+public:
+ static const ID none_id=-1;
+ ManuProcEntity() : entityid(none_id) {}
+ ManuProcEntity(const ID i) : entityid(i) {}
+ virtual ID Id() const = 0;
+
+};
+
+#endif
