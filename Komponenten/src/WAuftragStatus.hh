a08d139011f47ae4f8aa7181602c51d2b93b0865
christof <christof>
2002-09-18 08:50:35 +0000

    *** empty log message ***

--- Komponenten/src/WAuftragStatus.hh
+++ Komponenten/src/WAuftragStatus.hh
@@ -1,4 +1,4 @@
-// $Id: WAuftragStatus.hh,v 1.13 2002/09/18 08:54:40 christof Exp $
+// $Id: WAuftragStatus.hh,v 1.11 2002/09/18 08:50:35 christof Exp $
 /*  libKomponenten: ManuProC's Widget library
  *  Copyright (C) 2002 Adolf Petig GmbH & Co. KG
  *  written by Jacek Jakubowski, Christof Petig, Malte Thoma
--- Komponenten/src/WPreis.hh
+++ Komponenten/src/WPreis.hh
@@ -1,4 +1,4 @@
-// $Id: WPreis.hh,v 1.14 2002/09/18 08:50:35 christof Exp $
+// $Id: WPreis.hh,v 1.16 2002/09/18 08:54:40 christof Exp $
 /*  libKomponenten: ManuProC's Widget library
  *  Copyright (C) 2002 Adolf Petig GmbH & Co. KG
  *  written by Jacek Jakubowski, Christof Petig, Malte Thoma
