0db5973fa93789669d02464ab1cdd4cf09f59329
christof <christof>
2002-11-22 12:05:23 +0000

    *** empty log message ***

---
+++ Komponenten/Basic/src/RowDataBase.h
@@ -0,0 +1,45 @@
+/* $Id: RowDataBase.h,v 1.1 2002/11/22 12:05:23 christof Exp $ */
+/*  libKomponenten: GUI components for ManuProC's libcommon++
+ *  Copyright (C) 2001 Adolf Petig GmbH & Co. KG, written by Jacek Jakubowski
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+// $Id: RowDataBase.h,v 1.1 2002/11/22 12:05:23 christof Exp $
+
+#ifndef KOMPONENTEN_ROWDATA_H
+#define KOMPONENTEN_ROWDATA_H
+
+#include <Misc/Handles.h>
+#include <Misc/EntryValueBase.h>
+#include <glib.h>
+#include <cassert>
+
+class RowDataBase : public HandleContent
+{
+public:
+ virtual const cH_EntryValue Value(guint _seqnr,gpointer _g) const=0;
+ virtual ~RowDataBase(){}
+};
+
+class cH_RowDataBase : public Handle<const RowDataBase>
+{
+protected:
+ cH_RowDataBase() {}
+public:
+ cH_RowDataBase(const RowDataBase *r) : Handle<const RowDataBase>(r){}
+};
+
+#endif
---
+++ Komponenten/Basic/src/SimpleTree.cc
@@ -0,0 +1,28 @@
+// $Id: SimpleTree.cc,v 1.1 2002/11/22 12:05:23 christof Exp $
+/*  libKomponenten: GUI components for ManuProC's libcommon++
+ *  Copyright (C) 2002 Adolf Petig GmbH & Co. KG, written by Christof Petig
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <SimpleTree.hh>
+
+SimpleTree::SimpleTree(unsigned int cols,int attrs)
+	: SimpleTreeStore_Proxy(cols,attrs)
+{  set_model(sts.m_refTreeStore);
+
+   for (unsigned int i=0;i<cols;++i)
+      append_column("",sts.m_columns.cols[0]);
+}
