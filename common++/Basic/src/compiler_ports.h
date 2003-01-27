c0f7167d58737306f95d9528d9f0e9d765750ae0
christof <christof>
2003-01-27 13:52:47 +0000

    __deprecated as in linux kernel

--- common++/Basic/src/compiler_ports.h
+++ common++/Basic/src/compiler_ports.h
@@ -1,4 +1,4 @@
-// $Id: compiler_ports.h,v 1.1 2003/01/17 10:21:45 christof Exp $
+// $Id: compiler_ports.h,v 1.2 2003/01/27 13:52:47 christof Exp $
 
 // this macros cater for the various incompatible compilers
 
@@ -6,4 +6,18 @@
 #  define NOTGCC295(x) x
 #else
 #  define NOTGCC295(x)
+#endif
+
+/*
+ * taken from include/linux/compiler.h
+ *
+ * Allow us to mark functions as 'deprecated' and have gcc emit a nice
+ * warning for each use, in hopes of speeding the functions removal.
+ * Usage is:
+ *              int __deprecated foo(void)
+ */
+#if ( __GNUC__ == 3 && __GNUC_MINOR__ > 0 ) || __GNUC__ > 3
+#define __deprecated    __attribute__((deprecated))
+#else
+#define __deprecated
 #endif
