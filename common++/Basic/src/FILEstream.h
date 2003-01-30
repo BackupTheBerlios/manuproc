ef73a11d30c5e02beb5a811b599f2f76c5c3e84e
christof <christof>
2003-01-30 17:14:15 +0000

    *** empty log message ***

--- common++/Basic/src/FILEstream.h
+++ common++/Basic/src/FILEstream.h
@@ -1,4 +1,4 @@
-// $Id: FILEstream.h,v 1.3 2003/01/30 17:14:56 christof Exp $
+// $Id: FILEstream.h,v 1.2 2003/01/30 17:14:15 christof Exp $
 /*  Midgard Character Generator
  *  Copyright (C) 2001 Christof Petig
  *
@@ -51,4 +51,5 @@ public:
 	}
 	void close() {}
 };
+#endif
 #endif
