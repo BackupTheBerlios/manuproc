319efa356412629a0a9662512fe2bf3b8c1ce848
christof <christof>
2003-01-30 17:12:56 +0000

    *** empty log message ***

--- common++/Basic/src/FILEstream.h
+++ common++/Basic/src/FILEstream.h
@@ -1,4 +1,4 @@
-// $Id: FILEstream.h,v 1.1 2003/01/30 16:56:54 christof Exp $
+// $Id: FILEstream.h,v 1.3 2003/01/30 17:14:56 christof Exp $
 /*  Midgard Character Generator
  *  Copyright (C) 2001 Christof Petig
  *
@@ -28,7 +28,7 @@ class FILEbuf : public std::streambuf
    typedef char char_type;
    typedef int int_type;
         
-   FILEbuf(FILE *o) : o(_o) {}
+   FILEbuf(FILE *_o) : o(_o) {}
 protected:
    int_type overflow(int_type c) 
       {  if (c!=-1)
@@ -49,6 +49,6 @@ public:
 	oFILEstream(FILE *o) : std::ostream(0), b(o)
 	{  this->init(&b);
 	}
+	void close() {}
 };
-#endif
 #endif
