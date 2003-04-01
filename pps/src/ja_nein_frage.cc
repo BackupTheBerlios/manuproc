f9337a41ef19137978ef7f8747a6da695e5df532
jacek <jacek>
2003-04-01 11:11:45 +0000

    *** empty log message ***

---
+++ pps/src/ja_nein_frage.cc
@@ -0,0 +1,17 @@
+// generated 2003/4/1 12:06:42 MEST by jacek@ijacek.jacek.de
+// using glademm V1.1.3f_cvs
+//
+// newer (non customized) versions of this file go to ja_nein_frage.cc_new
+
+// This file is for your program, I won't touch it again!
+
+#include "config.h"
+#include "ja_nein_frage.hh"
+
+
+
+ja_nein_frage::ja_nein_frage(const std::string t)
+{
+ label_text->set_text(t);
+}
+
---
+++ pps/src/ja_nein_frage.hh
@@ -0,0 +1,22 @@
+// generated 2003/4/1 12:06:42 MEST by jacek@ijacek.jacek.de
+// using glademm V1.1.3f_cvs
+//
+// newer (non customized) versions of this file go to ja_nein_frage.hh_new
+
+// you might replace
+//    class foo : public foo_glade { ... };
+// by
+//    typedef foo_glade foo;
+// if you didn't make any modifications to the widget
+
+#ifndef _JA_NEIN_FRAGE_HH
+#  include "ja_nein_frage_glade.hh"
+#  define _JA_NEIN_FRAGE_HH
+class ja_nein_frage : public ja_nein_frage_glade
+{
+
+public:
+ ja_nein_frage(const std::string text="Sind Sie sicher ?");
+  
+};
+#endif
