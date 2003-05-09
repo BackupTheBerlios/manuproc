2d40aa16bc847006572a23d4af5f6d8b0a6471a6
jacek <jacek>
2003-05-09 15:45:02 +0000

    *** empty log message ***

---
+++ pps/src/auftrag_copy.cc
@@ -0,0 +1,9 @@
+// generated 2003/5/9 17:37:46 MEST by jacek@ijacek.jacek.de
+// using glademm V1.1.3f_cvs
+//
+// newer (non customized) versions of this file go to auftag_copy.cc_new
+
+// This file is for your program, I won't touch it again!
+
+#include "config.h"
+#include "auftag_copy.hh"
---
+++ pps/src/auftrag_copy.hh
@@ -0,0 +1,18 @@
+// generated 2003/5/9 17:37:46 MEST by jacek@ijacek.jacek.de
+// using glademm V1.1.3f_cvs
+//
+// newer (non customized) versions of this file go to auftag_copy.hh_new
+
+// you might replace
+//    class foo : public foo_glade { ... };
+// by
+//    typedef foo_glade foo;
+// if you didn't make any modifications to the widget
+
+#ifndef _AUFTAG_COPY_HH
+#  include "auftag_copy_glade.hh"
+#  define _AUFTAG_COPY_HH
+class auftag_copy : public auftag_copy_glade
+{  
+};
+#endif
