794fbb35a8fbfe8d3dcc10cb1de3179a53b6a916
jacek <jacek>
2003-05-14 21:08:04 +0000

    *** empty log message ***

--- pps/src/auftrag_copy.cc
+++ pps/src/auftrag_copy.cc
@@ -7,15 +7,22 @@
 
 #include "config.h"
 #include "auftrag_copy.hh"
+#include <unistd.h>
+#include <sys/types.h>
+#include "auftrag_bearbeiten.hh"
+#include "MyMessage.h"  
 
+extern auftrag_bearbeiten *auftragbearbeiten;
+extern MyMessage *meldung;
 
-auftrag_copy::auftrag_copy(const AuftragFull &auftrag)
+auftrag_copy::auftrag_copy(AuftragFull *auftrag)
+: alt_auftrag(auftrag)
 {
- alt_aufnr->set_text(itos(auftrag.Id()));
- alt_aufdatum->set_value(auftrag.getDatum());
+ alt_aufnr->set_text(itos(alt_auftrag->Id()));
+ alt_aufdatum->set_value(alt_auftrag->getDatum());
  alt_aufdatum->setLabel("");
- alt_aufkunde->set_value(auftrag.getKundennr());
- neu_aufkunde->set_value(auftrag.getKundennr());
+ alt_aufkunde->set_value(alt_auftrag->getKundennr());
+ neu_aufkunde->set_value(alt_auftrag->getKundennr());
  neu_lieferdatum->setLabel("");
  neu_lieferdatum->set_value(ManuProC::Datum());
   
@@ -25,6 +32,38 @@ auftrag_copy::auftrag_copy(const AuftragFull &auftrag)
 
 void auftrag_copy::on_copy_ok_clicked()
 {  
+ try {
+   AuftragFull *auftrag = new AuftragFull(Auftrag::Anlegen(
+   		alt_auftrag->Instanz()),
+   	neu_aufkunde->get_value());
+     auftrag->setYourAufNr(neu_youraufnr->get_text());
+
+     if(auftrag->getKundennr() == alt_auftrag->getKundennr())
+       {auftrag->Zahlart(alt_auftrag->Zahlart()->Id());
+        auftrag->setRabatt(alt_auftrag->getAuftragsRabatt().as_float());
+       }
+     else
+       {cH_Kunde k(auftrag->getKundennr());
+        auftrag->Zahlart(k->zahlungsart()->Id());     
+        if(alt_auftrag->getAuftragsRabatt()!=0)
+          auftrag->setRabatt(k->rabatt().as_float());
+       }
+
+
+	int stueck=stueck_uebernehmen.activate() ? neu_stueck:0;
+	ManuProC::Datum ld=liefdate_uebernehemen.activate() ? neu_liefdate
+
+
+
+      auftrag->setStatusAuftragFull((AufStatVal)OPEN,getuid()); 
+      auftragbearbeiten->new_aufid_from_copy=auftrag->Id();
+      }
+      
+ catch(SQLerror &e)
+   {meldung->Show(e);
+    auftragbearbeiten->new_aufid_from_copy=AuftragBase::none_id;
+    return;
+   }
 }
 
 void auftrag_copy::on_copy_cancel_clicked()
