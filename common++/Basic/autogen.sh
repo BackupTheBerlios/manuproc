cc79f8b13d0f781552fff99dbcd5853c7558b9a9
christof <christof>
2003-06-04 11:14:55 +0000

    force new autotool versions

--- common++/Basic/autogen.sh
+++ common++/Basic/autogen.sh
@@ -1,13 +1,13 @@
 libtoolize --force --copy
-autoheader
+autoheader2.50
 if [ -d macros -a ! -f /usr/share/aclocal/petig.m4 ]
 then
-  aclocal -I macros
+  aclocal-1.7 -I macros
 else
-  aclocal
+  aclocal-1.7
 fi
-automake --add-missing --copy --gnu
-autoconf
+automake-1.7 --add-missing --copy --gnu
+autoconf2.50
 ./configure $*
 make
             
