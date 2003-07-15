bef6f68c71cadf7e177125c482adc9c0f2a392a2
christof <christof>
2003-07-15 08:27:41 +0000

    *** empty log message ***

--- common++/tests/AuftragsTest/steuerprogramm.cc
+++ common++/tests/AuftragsTest/steuerprogramm.cc
@@ -1021,7 +1021,11 @@ int main(int argc,char *argv[])
    if (verbose) kill_output="";
 
    if (clean_only)
-      system((std::string("./clean_db ")+getenv("PGDATABASE")+kill_output).c_str());
+   {  system((std::string("./clean_db ")+getenv("PGDATABASE")+kill_output).c_str());
+      std::string cmd=std::string(MANU_DATAPATH)+"/clean_db2";
+      if (!access(cmd.c_str(),X_OK))
+         system((cmd+" "+getenv("PGDATABASE")+kill_output).c_str());
+   }
    else
       system((std::string(MANU_DATAPATH)+"/initdb.script "+MANU_DATAPATH+kill_output).c_str());
    std::cout << "...beendet\n";
