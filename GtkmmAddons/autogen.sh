1e11d7cf1f968c68f6941a405421df8fe71a9ab1
christof <christof>
2003-11-07 07:52:35 +0000

    new featurefull autogen.sh

--- GtkmmAddons/autogen.sh
+++ GtkmmAddons/autogen.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# $Id: autogen.sh,v 1.2 2003/11/03 16:40:23 christof Exp $
+
+if test ! -f install-sh ; then touch install-sh ; fi
 
 MAKE=`which gnumake`
 if test ! -x "$MAKE" ; then MAKE=`which gmake` ; fi
@@ -7,18 +8,43 @@ if test ! -x "$MAKE" ; then MAKE=`which make` ; fi
 HAVE_GNU_MAKE=`$MAKE --version|grep -c "Free Software Foundation"`
 
 if test "$HAVE_GNU_MAKE" != "1"; then 
-echo !!!! Warning: not tested with non Gnu-Make $MAKE
+echo Only non-GNU make found: $MAKE
 else
-echo Found GNU Make at $MAKE ... good.
+echo `$MAKE --version | head -1` found
 fi
 
-echo This script runs configure and make...
-echo You did remember necessary arguments for configure, right?
+if which autoconf2.50 >/dev/null
+then AC_POSTFIX=2.50
+elif which autoconf >/dev/null
+then AC_POSTFIX=""
+else 
+  echo 'you need autoconfig (2.58+ recommended) to generate the Makefile'
+  exit 1
+fi
+echo `autoconf$AC_POSTFIX --version | head -1` found
 
-libtoolize --force --copy 
-autoheader 
-aclocal 
-automake --add-missing --copy --gnu 
-autoconf 
-./configure $*
-$MAKE
+if which automake-1.7 >/dev/null
+then AM_POSTFIX=-1.7
+elif which automake-1.6 >/dev/null
+then AM_POSTFIX=-1.6
+elif which automake >/dev/null
+then AM_POSTFIX=""
+else
+  echo 'you need automake (1.7.8+ recommended) to generate the Makefile'
+  exit 1
+fi
+echo `automake$AM_POSTFIX --version | head -1` found
+
+echo This script runs automake/-conf, configure and make...
+echo You did remember necessary arguments for configure, right?
+echo
+
+libtoolize --force --copy
+autoheader$AC_POSTFIX
+if test -f /usr/share/aclocal/petig.m4
+then aclocal$AM_POSTFIX
+else aclocal$AM_POSTFIX -I macros
+fi
+automake$AM_POSTFIX --add-missing --copy --gnu
+autoconf$AC_POSTFIX
+./configure $* && $MAKE
