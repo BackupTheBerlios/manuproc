5216e753d41ee27942d4a7ffdaaf0811785232ce
jacek <jacek>
2003-12-11 08:40:55 +0000

    *** empty log message ***

--- GtkmmAddons/src/Gtk2TeX.h
+++ GtkmmAddons/src/Gtk2TeX.h
@@ -16,7 +16,7 @@
  *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-// $Id: Gtk2TeX.h,v 1.6 2003/11/12 10:35:29 christof Exp $
+// $Id: Gtk2TeX.h,v 1.7 2003/12/11 08:40:55 jacek Exp $
 
 #include <TeX.h>
 #include <iostream>
@@ -28,6 +28,7 @@ namespace Gtk2TeX
 	typedef std::string (*ColumnType_cb)(int col,const std::string &typ,const std::string &title,gpointer user_data);
 	typedef std::string (*ColumnTitle_cb)(int col,const std::string &title,gpointer user_data);
 	typedef std::string (*Element_cb)(const Gtk::TreeModel::const_iterator &r,int col,const std::string &text,gpointer user_data);
+	typedef std::string (*FirstRow_cb)(gpointer user_data, int deep, std::deque<guint> seq);
 	typedef void (*Row_cb)(std::ostream &o,const Gtk::TreeModel::const_iterator &r,gpointer user_data);
 	struct TableFlags
 	{	bool longtable:1;
@@ -37,7 +38,10 @@ namespace Gtk2TeX
 		ColumnType_cb columntype_cb;
 		Row_cb preline_cb,postline_cb;
 		Simple_cb prehead_cb,posthead_cb,postlist_cb;
-		
+		FirstRow_cb firstrow_cb;
+		int deep;
+		std::deque<guint> sequence; // sequence of cols
+
 		// e.g. by tv->get_selection()->get_selected(tv->get_model());
 		Gtk::TreeModel::const_iterator selection;
 		// Glib::RefPtr<Gtk::TreeSelection> selection;
@@ -52,6 +56,7 @@ namespace Gtk2TeX
 		  multicolumn(0),
 		  columntype_cb(0), preline_cb(0), postline_cb(0),
 		  prehead_cb(0), posthead_cb(0), postlist_cb(0),
+                  firstrow_cb(0),
 //		  first_line(0), last_line(-1),
 		  user_data(0), columntitle_cb(0),
 		  element_cb(0)
