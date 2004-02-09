0e483a5e811a351c51338d5110b826701e79c049
jacek <jacek>
2004-02-09 21:44:06 +0000

    *** empty log message ***

---
+++ pps/src/buchen_dialog.cc
@@ -0,0 +1,9 @@
+// generated 2004/2/7 19:56:13 CET by jacek@jaceksdell.(none)
+// using glademm V2.0.0b
+//
+// newer (non customized) versions of this file go to buchen_dialog.cc_new
+
+// This file is for your program, I won't touch it again!
+
+#include "config.h"
+#include "buchen_dialog.hh"
---
+++ pps/src/buchen_dialog.hh
@@ -0,0 +1,18 @@
+// generated 2004/2/7 19:56:13 CET by jacek@jaceksdell.(none)
+// using glademm V2.0.0b
+//
+// newer (non customized) versions of this file go to buchen_dialog.hh_new
+
+// you might replace
+//    class foo : public foo_glade { ... };
+// by
+//    typedef foo_glade foo;
+// if you didn't make any modifications to the widget
+
+#ifndef _BUCHEN_DIALOG_HH
+#  include "buchen_dialog_glade.hh"
+#  define _BUCHEN_DIALOG_HH
+class buchen_dialog : public buchen_dialog_glade
+{  
+};
+#endif
