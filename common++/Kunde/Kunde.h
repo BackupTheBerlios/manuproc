09758794eea847f11fbb3ffce4e3ff15263d3015
jacek <jacek>
2004-03-19 16:17:16 +0000

    *** empty log message ***

--- common++/Kunde/Kunde.h
+++ common++/Kunde/Kunde.h
@@ -1,4 +1,4 @@
-// $Id: Kunde.h,v 1.58 2004/01/21 13:00:17 jacek Exp $
+// $Id: Kunde.h,v 1.59 2004/03/19 16:17:16 jacek Exp $
 /*  libcommonc++: ManuProC's main OO library
  *  Copyright (C) 1998-2000 Adolf Petig GmbH & Co. KG, written by Christof Petig
  *
@@ -413,13 +413,14 @@ friend enum Kunde::B_UPDATE_BITS_SONST &operator++(enum Kunde::B_UPDATE_BITS_SON
 
 #ifdef MABELLA_EXTENSIONS
 private:
-	mutable std::string cab, abi; // RiBa Kontobeschreibung; Bankeinzug in Italy
+	mutable std::string cab, abi, acc; // RiBa Kontobeschreibung; Bankeinzug in Italy
 	mutable std::string iban;  // nr für LCR Verfahren (z.B. Frankreich)
 	mutable std::string bankname; // Name der Bank in Italien 
 	void init_AuslandZahlung() const throw(SQLerror);	
 public:
 	std::string getABI_Code(bool force=false) const;
 	std::string getCAB_Code(bool force=false) const;	
+	std::string getRiBaACC(bool force=false) const;
 	std::string getIBAN_Code(bool force=false) const;
 	std::string getABIBankName(bool force=false) const;
 	void setABI_CAB(const std::string abi, const std::string cab) const throw(SQLerror);
