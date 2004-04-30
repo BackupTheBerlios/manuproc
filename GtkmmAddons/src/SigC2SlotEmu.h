fd8430d391ae796476d01724c370cfa0d1a467a0
christof <christof>
2004-04-30 13:31:27 +0000

    this covers most uses

--- GtkmmAddons/src/SigC2SlotEmu.h
+++ GtkmmAddons/src/SigC2SlotEmu.h
@@ -16,7 +16,7 @@
  *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-// $Id: SigC2SlotEmu.h,v 1.2 2004/04/29 16:45:41 christof Exp $
+// $Id: SigC2SlotEmu.h,v 1.3 2004/04/30 13:31:27 christof Exp $
 
 #ifndef F8A1CFD6_43A4_4DBC_9F30_826BBC4F0650
 #define F8A1CFD6_43A4_4DBC_9F30_826BBC4F0650
@@ -25,9 +25,15 @@
 #if GTKMM_MAJOR_VERSION==2 && GTKMM_MINOR_VERSION>2
 // don't ask me why this does not work ...
 //#  include <sigc++/compatibility.h>
+
+#include <sigc++/slot.h>
+#include <sigc++/bind.h>
+
 namespace SigC
 {  template <class T> 
     Slot0<void> slot(T &t, void (T::*p)()) { return sigc::mem_fun(t,p); }
+   template <class T,class U> 
+    Slot0<void> slot(T &t, void (U::*p)()) { return sigc::mem_fun(t,p); }
    template <class T,class A> 
     Slot1<void,A> slot(T &t, void (T::*p)(A a)) 
     	{ return sigc::mem_fun(t,p); }
