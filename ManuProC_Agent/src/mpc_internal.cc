f4244d80b93c5ab707ecada4565b8e48c084c5ac
jacek <jacek>
2004-05-13 14:33:33 +0000

    *** empty log message ***

--- ManuProC_Agent/src/mpc_internal.cc
+++ ManuProC_Agent/src/mpc_internal.cc
@@ -11,6 +11,9 @@ void mpc_agent::on_orderid_search(gboolean *cont,GtkSCContext context)
    {  
     std::string squery("select aufid from auftrag where vknr=?");
 
+    if(!orderid->get_text().empty())
+      squery=squery+" and aufid like '"+orderid->get_text()+"%'";
+
     if(!kunde->get_value(KDBOX_NR).empty())
        squery+=std::string(" and kdnr=?");
 
@@ -34,7 +37,7 @@ void mpc_agent::on_orderid_search(gboolean *cont,GtkSCContext context)
 	    FetchIStream fi=qu.Fetch();
  	    while(fi.good())
 	      {fi >> aufid;
-	       orderid->add_item(itos(VERKNR)+'_'+itos(aufid),aufid);
+	       orderid->add_item(itos(aufid),aufid);
 	       fi=qu.Fetch();
 	      }
 	    *cont=false;
@@ -55,13 +58,27 @@ void mpc_agent::on_orderid_search(gboolean *cont,GtkSCContext context)
 }
 
 
+void mpc_agent::artikel_clean()
+{
+ article_entry->reset();
+ width_entry->reset();
+ color_entry->reset();
+ makeup_entry->reset();
+ ean_entry->reset();
+ 
+ article_entry->set_sensitive(true);
+ width_entry->set_sensitive(true);
+ color_entry->set_sensitive(true);
+ makeup_entry->set_sensitive(true); 
+ ean_entry->set_sensitive(true);
+}
 
 void mpc_agent::clear_order()
 {
  orderid->reset();
  orderid->set_sensitive(true);
  label_orderdate->set_text("");
- artikel->reset();
+ artikel_clean();
  menge->set_value(0);
  order->clear();
 }
@@ -82,7 +99,7 @@ void mpc_agent::load_order(int oid)
    {kunde->set_value(KDBOX_NR,kdnr);
     on_activate_entry(KDBOX_NR);  
    }
- orderid->set_value(itos(VERKNR)+"_"+itos(oid),oid);
+ orderid->set_value(itos(oid),oid);
  orderid->set_sensitive(false);
 }
 
@@ -120,4 +137,70 @@ int mpc_agent::create_new_order(int kdnr)
 }
 
 
+void mpc_agent::on_ean_entry_search(gboolean *cont,GtkSCContext context)
+{  
+}
+
+void mpc_agent::on_article_entry_search(gboolean *cont,GtkSCContext context)
+{
+ try
+   {  
+    std::string squery("select artnr from artikel");
+
+    if(!orderid->get_text().empty())
+      squery=squery+" where 1=? and artnr like '"+article_entry->get_text()+"%'";
+
+    Query qu(squery);
+
+    switch(context)
+	{  
+         case GTK_SEARCH_OPEN:
+	 case GTK_SEARCH_REOPEN:
+           {
+	    qu << 1; // only to trigger a Query at this place;
+	    // fall through
+	   }
+
+	 case GTK_SEARCH_FETCH:
+	   // dont need idle loops here. It is rapid enouth from SQLite.
+	   {
+	    std::string artikel;
+	    FetchIStream fi=qu.Fetch();
+ 	    while(fi.good())
+	      {fi >> artikel;
+	       article_entry->add_item(artikel,artikel);
+	       fi=qu.Fetch();
+	      }
+	    *cont=false;
+	    break;
+	   }
+
+	 case GTK_SEARCH_CLOSE:
+           {
+            break;
+           }
+
+	}
+   }
+  catch (SQLerror &e)
+   {  std::cerr << e << '\n';
+   }
+  
+}
+
+
+
+void mpc_agent::on_width_entry_search(gboolean *cont,GtkSCContext context)
+{  
+}
+
+
+void mpc_agent::on_color_entry_search(gboolean *cont,GtkSCContext context)
+{  
+}
+
+
+void mpc_agent::on_makeup_entry_search(gboolean *cont,GtkSCContext context)
+{  
+}
 
