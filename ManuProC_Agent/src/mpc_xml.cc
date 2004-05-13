eea93f13a4e8d314d5e43676b2500b213f5b9662
jacek <jacek>
2004-05-13 11:49:26 +0000

    *** empty log message ***

--- ManuProC_Agent/src/mpc_xml.cc
+++ ManuProC_Agent/src/mpc_xml.cc
@@ -2,15 +2,21 @@
 #include "mpc_agent.hh"
 #include <TagStream.h>
 #include <MyMessage.h>
-
+#include <itos.h>
 
 void mpc_agent::on_senden_clicked()
 {
  std::string file="/dev/stdout";
+ if(orderid->sensitive()) return; //nix selectiert
+ std::string ord_id=itos(orderid->get_value());
+
 
  TagStream ts;
  ts.setEncoding("ISO-8859-1");
  Tag &data=ts.push_back(Tag("ManuProC-Order"));
+ data.setAttr("origin","mpc_agent");
+ data.setAttr("orderid",ord_id);
+ data.push_back(Tag("test")); 
 
  std::ofstream of(file.c_str());
  ts.write(of);
