36fdd865ad603e962acd08d616f153dc7c260e84
jacek <jacek>
2004-10-13 10:10:44 +0000

    *** empty log message ***

--- common++/DB/functions/auftrag_status.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef AUFSTATUS
-#define AUFSTATUS
-
-typedef enum {UNCOMMITED=0, OPEN, CLOSED, STORNO} AufStatVal;
-typedef enum {REQUEST=0, CONFIRM, PROBLEM} AufChangeStatVal;
-typedef enum {MENGE='M', LIEFDATUM='D'} AufChangeTypeVal;
-
-#endif
