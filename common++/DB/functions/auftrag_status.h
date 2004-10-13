bf9fdffed8a8f47391a6568cea6b64d33ddd9a7e
jacek <jacek>
2004-10-13 10:06:01 +0000

    *** empty log message ***

--- common++/DB/functions/auftrag_status.h
+++ common++/DB/functions/auftrag_status.h
@@ -1,28 +1,9 @@
-/* $Id: auftrag_status.h,v 1.3 2004/10/13 10:10:44 jacek Exp $ */
-/*  libcommonc++: ManuProC's main OO library
- *  Copyright (C) 2001 Adolf Petig GmbH & Co. KG, written by Jacek Jakubowski
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- */
-
 
 #ifndef AUFSTATUS
 #define AUFSTATUS
 
-typedef enum {NOSTAT=-1, UNCOMMITED=0, OPEN, CLOSED, STORNO} AufStatVal;
+typedef enum {UNCOMMITED=0, OPEN, CLOSED, STORNO} AufStatVal;
 typedef enum {REQUEST=0, CONFIRM, PROBLEM} AufChangeStatVal;
-typedef enum {MENGE='M', LIEFDATUM='D', STATUS='S'} AufChangeTypeVal;
+typedef enum {MENGE='M', LIEFDATUM='D'} AufChangeTypeVal;
 
 #endif
