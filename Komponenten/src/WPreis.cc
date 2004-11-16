9b8436fc2f0640c8c4767e806d32793296168b60
christof <christof>
2004-11-16 11:46:46 +0000

    *** empty log message ***

---
+++ Komponenten/src/WPreis.cc
@@ -0,0 +1,147 @@
+// $Id: WPreis.cc,v 1.4 2004/11/16 11:46:46 christof Exp $
+/*  libKomponenten: ManuProC's Widget library
+ *  Copyright (C) 2002 Adolf Petig GmbH & Co. KG
+ *  written by Jacek Jakubowski, Christof Petig, Malte Thoma
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include "WPreis.hh"
+//#include <Aux/Transaction.h>
+//#include <Aux/SQLerror.h>
+#include <gtk--/adjustment.h>
+
+// strictly these are part of the ctor
+
+gint WPreis::try_grab_focus(GtkWidget *w,gpointer gp)
+{ 
+   WPreis *this2((WPreis*)gp);
+   assert(Gtk::Table::isA(this2)); // very weak check
+   this2->SP->grab_focus();
+   this2->SP->select_region(0,-1);
+   return true;
+}
+
+void WPreis::betrag()
+{
+  Gtk::Adjustment *SP_adj = manage(new class Gtk::Adjustment(0, 0, 10000, 1, 10, 10));
+  SP=manage(new class Gtk::SpinButton(*SP_adj, 1, 
+		ManuProC::Precision::GeldBetrag));
+  SP->set_usize(80,-1);
+  SP->show();
+  SP->activate.connect(activate.slot());
+  SP->changed.connect(preis_changed.slot());
+  attach(*SP,1,2,0,1);
+}
+void WPreis::waehrung()
+{
+  if (wwaehrung_bool)
+   {
+     WW=manage(new WWaehrung(WWaehrung::KURZ));
+     WW->activate.connect(activate.slot());
+     attach(*WW,2,3,0,1);
+   }
+  else
+   {
+     LA=manage(new Gtk::Label("?"));
+     LA->set_alignment(0, 0.5);
+     LA->show();
+     attach(*LA,2,3,0,1);
+   }
+}
+void WPreis::preismenge()
+{
+  Gtk::Label *LAp=manage(new Gtk::Label("per"));
+  LAp->set_justify(GTK_JUSTIFY_RIGHT);
+  LAp->show();
+  attach(*LAp,0,1,1,2);
+  Gtk::Adjustment *SP_adj = manage(new class Gtk::Adjustment(1, 0, 10000, 1, 10, 10));
+  SP2=manage(new class Gtk::SpinButton(*SP_adj, 3, 0));
+  SP2->set_usize(80,-1);
+  SP2->show();
+  SP2->activate.connect(activate.slot());
+  attach(*SP2,1,2,1,2);
+}
+
+void WPreis::einheit()
+{
+  LA2=manage(new Gtk::Label(std::string(einh)));
+  LA2->set_alignment(0, 0.5);
+  LA2->show();
+  attach(*LA2,2,3,1,2);
+}
+
+void WPreis::mindmenge()
+{
+ LabMindMenge=manage(new Gtk::Label("ab "+
+		itos(mindestmenge)+" "+LA2->get_text()));
+ LabMindMenge->set_justify(GTK_JUSTIFY_RIGHT);
+ LabMindMenge->set_alignment(0, 0.5);
+ LabMindMenge->show();
+ attach(*LabMindMenge,0,1,0,1);
+}
+
+WPreis::WPreis(bool wwaehrung)
+: SP(), SP2(), LA(), LA2(), WW(), LabMindMenge(),
+	mindestmenge(1), wwaehrung_bool(wwaehrung),
+	einh(Einheit::default_id)
+{
+  betrag();
+  waehrung();
+  preismenge();
+  einheit(); 
+  mindmenge();
+  set_Waehrung(Waehrung::default_id);
+  gtk_signal_connect_after(GTK_OBJECT(gtkobj()), "grab_focus",GTK_SIGNAL_FUNC (&try_grab_focus),(gpointer)this);
+}
+
+void WPreis::update()
+{
+  gtk_spin_button_update(SP->gtkobj());
+  gtk_spin_button_update(SP2->gtkobj());
+}
+
+void WPreis::reset()
+{ set_value(Preis());
+}
+
+void WPreis::set_value(const Preis &p,int _mindmenge)
+{ SP->set_value(p.Wert().as_float());
+  SP2->set_value(p.BezugsMenge().as_float());
+  set_Waehrung(p.getWaehrung());
+  set_Mindmenge(_mindmenge);
+}
+
+void WPreis::set_value(const Artikelpreis &ap)
+{ SP->set_value(ap.getPreis().Wert().as_float());
+  SP2->set_value(ap.getPreis().BezugsMenge().as_float());
+  set_Waehrung(ap.getPreis().getWaehrung());
+  set_Mindmenge(ap.MindMenge());
+}
+
+void WPreis::set_Waehrung(const cP_Waehrung &w) 
+{ chwaehrung=w;
+  if (wwaehrung_bool)  WW->set_value(w);
+  else LA->set_text(w->Kurzbezeichnung());
+}
+
+cP_Waehrung WPreis::getWaehrung() const
+{ return wwaehrung_bool?WW->get_value():chwaehrung;
+}
+
+const Preis WPreis::get_value() const
+{ const_cast<WPreis*>(this)->update();
+  return Preis(SP->get_value_as_float(),getWaehrung(),SP2->get_value_as_float());
+}
---
+++ Komponenten/src/WPreis.hh
@@ -0,0 +1,104 @@
+// $Id: WPreis.hh,v 1.18 2004/11/16 11:46:46 christof Exp $
+/*  libKomponenten: ManuProC's Widget library
+ *  Copyright (C) 2002 Adolf Petig GmbH & Co. KG
+ *  written by Jacek Jakubowski, Christof Petig, Malte Thoma
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef _WPREIS_HH
+#  define _WPREIS_HH
+
+#include <gtk--/table.h>
+#include <Aux/Waehrung.h>
+#include <gtk--/spinbutton.h>
+#include <gtk--/label.h>
+#include "WWaehrung.hh"
+#include <Artikel/Preis.h>
+#include <Artikel/Artikelpreis.h>
+#include <Artikel/Einheiten.h>
+#include <Aux/itos.h>
+
+class WPreis : public Gtk::Table 
+{
+      Gtk::SpinButton *SP,*SP2;
+      Gtk::Label *LA,*LA2;
+      WWaehrung *WW;
+      Gtk::Label *LabMindMenge;
+      int mindestmenge;
+
+
+      bool wwaehrung_bool;
+      Einheit einh;	
+      cP_Waehrung chwaehrung;
+
+      // erzeugt die entsprechenden Unterwidgets
+      void betrag();
+      void waehrung();
+      void preismenge();
+      void einheit();
+      void mindmenge();
+
+	static gint try_grab_focus(GtkWidget *w,gpointer gp);
+
+   public:
+      void update();
+      WPreis(bool wwaehrung=true);
+      ~WPreis(){}
+
+      SigC::Signal0<void> activate;
+      SigC::Signal0<void> preis_changed;
+      
+      void set_value(const Preis &p,int _mindm=1);
+      void set_value(const Artikelpreis &ap);
+      const Preis get_value() const;
+      void reset();
+      
+      void set_Einheit(const Einheit &e)
+      { einh=e; 
+	LA2->set_text(std::string(einh)); 
+	set_Mindmenge(mindestmenge);
+	}
+
+// bitte nur mit gutem Grund verwenden
+      void set_Waehrung(const cP_Waehrung &w);
+      cP_Waehrung getWaehrung() const;
+
+// veraltet
+      void set_all(Preis::pfennig_cent_t d1, Preis::preismenge_t d2) // und Waehrung?
+      {  set_value(Preis(d1,getWaehrung(),d2));
+      }
+      void set_Betrag(Preis::pfennig_cent_t d){SP->set_value(d.as_float());}
+      void set_Preismenge(Preis::preismenge_t d){SP2->set_value(d.as_float());}
+private: // echt zu alt
+      void set_Einheit(const std::string s){LA2->set_text(s);}
+public:
+      void set_Mindmenge(int _mind)
+		{mindestmenge=_mind;
+		 LabMindMenge->set_text("ab "+
+			itos(mindestmenge)+" "+LA2->get_text());
+		}
+
+      Preis::pfennig_cent_t get_Betrag() const 
+      {  return get_value().Wert(); }
+      Preis::preismenge_t get_Preismenge() const 
+      {  return get_value().BezugsMenge(); }
+      WaehrungID::enum_t get_Waehrung_enum() const 
+      {  return getWaehrung()->Id();}
+      const Preis get_Preis() const 
+      {  return get_value(); }
+      int Mindmenge() const { return mindestmenge; }
+};
+#endif
