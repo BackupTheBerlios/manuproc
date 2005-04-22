9f41e4d2d64c0b040346976a7c1897befc7a39ac
christof <christof>
2005-04-22 17:11:00 +0000

    correct database scheme

--- ManuProC_Agent/src/mpc.sql
+++ ManuProC_Agent/src/mpc.sql
@@ -1,7 +1,24 @@
 BEGIN TRANSACTION;
+create table artikel (artnr text not null,breite text not null, farbe text not null, aufmachung text not null, ean text, bezeichnung text, unique (ean), unique (artnr,breite,farbe,aufmachung));
+INSERT INTO artikel VALUES(2021,25,360,25,4031197041435,'Scarlett');
+INSERT INTO artikel VALUES(2021,25,430,25,4031197041763,'Scarlett');
+INSERT INTO artikel VALUES(2021,40,360,25,4031197041428,'Scarlett');
+INSERT INTO artikel VALUES(2522,25,505,20,4031197041916,'Corona');
+INSERT INTO artikel VALUES(2522,40,505,20,4031197041923,'Corona');
 create table auftrag (aufid int not null, kdnr int not null, vknr int not null,datum timestamp not null, primary key (aufid));
-create table auftragentry (aufid int not null, vknr int not null,artikelid int not null,stueck int not null,preis numeric(6,2));
+INSERT INTO auftrag VALUES(1,1,0,'2004-5-11');
+create table auftragentry (aufid int not null, 
+vknr int not null,
+artnr text not null,
+breite text not null,
+farbe text not null,
+aufmachung text not null,
+ean text unique,
+stueck int not null,preis numeric(6,2),
+unique (artnr,breite,farbe,aufmachung));
+INSERT INTO auftragentry VALUES(1,0,2020,10,210,50,4031197001309,10,NULL);
+INSERT INTO auftragentry VALUES(1,0,2015,100,100,20,4031197019489,12,NULL);
 create table kunden (kundennr numeric(5) primary key,name text not null,ort text);
-create table artikel (artnr int not null, breite int not null, farbe int not null, aufmachung int not null, ean longint, bezeichnung text);
-insert into kunden values (9999,'Testkunde','Wuppertal');
+INSERT INTO kunden VALUES(1,'Test','Testort');
+INSERT INTO kunden VALUES(2,'Test2','Ort');
 COMMIT;
