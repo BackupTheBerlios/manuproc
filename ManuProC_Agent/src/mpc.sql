36ad1fc87eb2f52c38fd9674fb5b3747e5335279
christof <christof>
2005-04-22 17:10:41 +0000

    first attempt to fix the schema, gave up

--- ManuProC_Agent/src/mpc.sql
+++ ManuProC_Agent/src/mpc.sql
@@ -3,4 +3,5 @@ create table auftrag (aufid int not null, kdnr int not null, vknr int not null,d
 create table auftragentry (aufid int not null, vknr int not null,artikelid int not null,stueck int not null,preis numeric(6,2));
 create table kunden (kundennr numeric(5) primary key,name text not null,ort text);
 create table artikel (artnr int not null, breite int not null, farbe int not null, aufmachung int not null, ean longint, bezeichnung text);
+insert into kunden values (9999,'Testkunde','Wuppertal');
 COMMIT;
