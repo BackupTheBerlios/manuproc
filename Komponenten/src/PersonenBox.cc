bd230076c57671172d6114772f26e80daf16ad7f
christof <christof>
2005-06-24 11:12:33 +0000

    ZahlungsArtBox + PersonenBox

---
+++ Komponenten/src/PersonenBox.cc
@@ -0,0 +1,51 @@
+// $Id: PersonenBox.cc,v 1.6 2005/06/24 11:12:33 christof Exp $
+/*  libKomponenten: GUI components for ManuProC's libcommon++
+ *  Copyright (C) 2001 Adolf Petig GmbH & Co. KG, written by Jacek Jakubowski
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include "PersonenBox.hh"
+#include <Aux/itos.h>
+
+PersonenBox::PersonenBox()
+{
+  _tabelle_="kunden";
+  _string1_="name2";
+  _string2_="firma";
+  _int_="kundennr";
+  _none_id_=Kunde::none_id;
+  setLabel("Id","Nachname","Vorname");
+  show_string2(true);
+
+  Join(" join ku_gruppen_map m using (kundennr)");
+}
+
+void PersonenBox::set_gruppe(Kundengruppe::ID gid)
+{
+ Einschraenkung(" and m.grpnr="+itos(gid));
+}
+
+
+void PersonenBox::set_value(int i)
+{  cH_Kunde PL(i);
+   IntStringBox::set_value(PL->Id(),PL->getName(),PL->getName2());
+}
+
+cH_Kunde PersonenBox::get_value()
+{
+  return cH_Kunde(IntStringBox::get_value());
+}
+
---
+++ Komponenten/src/ZahlungsArtBox.hh
@@ -0,0 +1,43 @@
+// $Id: ZahlungsArtBox.hh,v 1.4 2005/06/24 11:12:34 christof Exp $
+/*  libKomponenten: GUI components for ManuProC's libcommon++
+ *  Copyright (C) 2001 Adolf Petig GmbH & Co. KG, written by Malte Thoma
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+// generated 2000/8/28 14:42:53 CEST by christof@puck.petig.de
+// using glademm V0.5.9a
+//
+// newer (non customized) versions of this file go to PreisBox.hh_glade
+
+// you might replace
+//    class Foo : public Foo_glade { ... };
+// by
+//    typedef Foo_glade Foo;
+// if you didn't make any modifications to the widget
+
+#ifndef _ZAHLUNGSARTBOXX_HH
+#  define _ZAHLUNGSARTBOXX_HH
+
+#include <IntStringBox.hh>
+
+class ZahlungsArtBox : public IntStringBox
+{	
+
+public:
+	ZahlungsArtBox();
+	void set_value(int i) ;
+};
+#endif
