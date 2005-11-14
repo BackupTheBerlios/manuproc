120387a8d2a60d5ed15fb0aad6fa04d4a6e700e8
christof <christof>
2005-11-14 07:57:12 +0000

    gettext #1 monotone christof@petig-baender.de 11 Nov 2005 22:25:17 +0000 d55ac3a60a17dd3e00e55dd030542f9f838859ce

--- kundendaten/src/kundendaten.cc
+++ kundendaten/src/kundendaten.cc
@@ -19,6 +19,11 @@ MyMessage *Message;
 
 int main(int argc, char **argv)
 {   
+#if defined(ENABLE_NLS)
+   bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
+   bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
+   textdomain (GETTEXT_PACKAGE);
+#endif //ENABLE_NLS
    Petig::PrintUncaughtExceptions();
    Petig::Connection conn;
    int i;
