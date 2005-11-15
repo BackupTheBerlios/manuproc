ac7e3ec075de9a37642e5588584696120f205fe0
christof <christof>
2005-11-15 09:56:19 +0000

    gettext monotone christof@petig-baender.de 15 Nov 2005 08:41:48 +0000 ea177b22064667c1d45b66ee71daa141fa8e2270

--- common++/autogen.sh
+++ common++/autogen.sh
@@ -41,12 +41,13 @@ echo This script runs automake/-conf, configure and make...
 echo You did remember necessary arguments for configure, right?
 echo
 
-libtoolize --force --copy
-autoheader$AC_POSTFIX
 if test -f /usr/share/aclocal/petig.m4
 then aclocal$AM_POSTFIX
 else aclocal$AM_POSTFIX -I macros
 fi
+libtoolize --force --copy
+gettextize
+autoheader$AC_POSTFIX
 automake$AM_POSTFIX --add-missing --copy --gnu
 autoconf$AC_POSTFIX
 ./configure $* && $MAKE
