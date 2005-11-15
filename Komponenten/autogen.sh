dca525f2d6cae5d988669396e79bc50a6ffa55b3
christof <christof>
2005-11-15 13:19:31 +0000

    guard against gettextize destroying configure.in or Makefile.am monotone christof@petig-baender.de 15 Nov 2005 12:46:23 +0000 f8b867ec69bcc3b26276b493b92720a382a9311a

--- Komponenten/autogen.sh
+++ Komponenten/autogen.sh
@@ -1,6 +1,8 @@
 aclocal
 libtoolize --force --copy
+tar cvzf backup.tgz configure.in Makefile.am
 gettextize
+tar xvzf backup.tgz
 if test ! -e po/Makevars
 then cp po/Makevars.template po/Makevars
 fi
