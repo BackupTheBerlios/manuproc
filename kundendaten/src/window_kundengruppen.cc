bfd33450294edb5cee993df6785d826d23c2664a
christof <christof>
2006-01-10 10:32:51 +0000

    überschriften monotone christof@petig-baender.de 02 Dec 2005 15:12:00 +0000 5d7a4f9a82b2892b73b9bfe9d7fb4d4529d30ee6

--- kundendaten/src/window_kundengruppen.cc
+++ kundendaten/src/window_kundengruppen.cc
@@ -1,7 +1,8 @@
-// $Id: window_kundengruppen.cc,v 1.1 2006/01/10 10:32:46 christof Exp $
+// $Id: window_kundengruppen.cc,v 1.2 2006/01/10 10:32:51 christof Exp $
 
 #include "config.h"
 #include "window_kundengruppen.hh"
+#include <Kunde/Kundengruppe.h>
 
 void window_kundengruppen::on_gruppenwahl_activate()
 {  
@@ -23,6 +24,38 @@ void window_kundengruppen::add()
 {  
 }
 
+void window_kundengruppen::laden()
+{ // .... 
+}
+
+enum kg_STcols
+{ SP_NR, SP_NAME, SP_VORNAME, SP_ORT, SP_LAND, SP_SORTNAME, SP_PLZ, 
+  SP_7, SP_8, SP_9,
+  SP_ANZ
+};
+
+template <bool B>
+struct kg_STprops : SimpleTreeModel_Properties
+{ unsigned Columns() const { return SP_ANZ; }
+  Glib::ustring Title(guint _seqnr) const
+  { switch(_seqnr)
+    { case SP_NR: return "Nr.";
+      case SP_NAME: return "Name";
+      case  SP_VORNAME: return "Vorname";
+      case  SP_ORT: return "Ort";
+      case  SP_LAND: return "Land";
+      case  SP_SORTNAME: return "Abkz.";
+      case  SP_PLZ: return "PLZ";
+      default: return "";
+    }
+  }
+  std::string ProgramName() const { return "kundendaten"; }
+  std::string InstanceName() const { return B?"gruppen1":"gruppen0"; }
+};
+
 window_kundengruppen::window_kundengruppen(int id)
-{
+{ gruppe->set_value(Kundengruppe::ID(id));
+  kundein->setProperties(*new kg_STprops<true>(),true);
+  anderekunden->setProperties(*new kg_STprops<false>(),true);
+  laden();
 }
--- kundendaten/src/window_kundengruppen.hh
+++ kundendaten/src/window_kundengruppen.hh
@@ -1,4 +1,4 @@
-// $Id: window_kundengruppen.hh,v 1.1 2006/01/10 10:32:46 christof Exp $
+// $Id: window_kundengruppen.hh,v 1.2 2006/01/10 10:32:51 christof Exp $
 
 #ifndef _WINDOW_KUNDENGRUPPEN_HH
 #  include "window_kundengruppen_glade.hh"
@@ -9,6 +9,7 @@ class window_kundengruppen : public window_kundengruppen_glade
         void loeschen();
         void entfernen();
         void add();
+        void laden();
 public:
   window_kundengruppen(int id);
 };
