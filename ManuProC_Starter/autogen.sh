0fc534c0dac950a46f593d442871f81d6faeae3c
christof <christof>
2006-05-17 07:39:44 +0000

    i18n monotone christof@petig-baender.de 11 May 2006 15:57:47 +0000 0393ad3c55701ecfe5dc754cbcdae59e6108ae84

--- ManuProC_Starter/autogen.sh
+++ ManuProC_Starter/autogen.sh
@@ -25,9 +25,18 @@ if test ! -x `which automake`
 then echo you need automake to generate the Makefile
 fi
 
-libtoolize --force --copy
-autoheader
 aclocal
+libtoolize --force --copy
+tar cvzf backup.tgz configure.in Makefile.am ChangeLog
+gettextize
+tar xvzf backup.tgz
+if test ! -e po/Makevars
+then cp po/Makevars.template po/Makevars
+fi
+if test ! -e po/LINGUAS
+then touch po/LINGUAS
+fi
+autoheader
 automake --add-missing --copy --gnu
 autoconf
 ./configure $* && $MAKE
--- ManuProC_Starter/src/manuproc_starter.cc
+++ ManuProC_Starter/src/manuproc_starter.cc
@@ -13,6 +13,11 @@
 
 int main(int argc, char **argv)
 {  
+#if defined(ENABLE_NLS)
+   bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
+   bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
+   textdomain (GETTEXT_PACKAGE);
+#endif //ENABLE_NLS
    
    Gtk::Main m(&argc, &argv);
 
