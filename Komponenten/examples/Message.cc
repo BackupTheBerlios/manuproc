01a73a753a7be846d591d14274c33c60b92f3862
christof <christof>
2006-10-31 16:10:19 +0000

    Modale Fenster mit get_toplevel monotone christof@petig-baender.de 02 Oct 2006 07:19:35 +0000 c08ca2ea26d52cb5d864e09f1fb5d4c1afe93abd

--- Komponenten/examples/Message.cc
+++ Komponenten/examples/Message.cc
@@ -1,7 +1,15 @@
 #include <MyMessage.h>
 #include <gtkmm/main.h>
+#include <gtkmm/label.h>
 
 int main(int argc, char **argv)
 { Gtk::Main m(argc,argv);
   MyMessage::show_and_wait("test");
+  
+  Gtk::Window w;
+  Gtk::Label l("X");
+  l.show();
+  w.add(l);
+  w.show();
+  MyMessage::show_and_wait("test X",l.get_toplevel());
 }
